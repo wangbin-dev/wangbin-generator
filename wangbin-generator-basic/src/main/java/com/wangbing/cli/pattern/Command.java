6bf5fdff8a6e9ad8904cfcda3e5291d75f37ec17
wangbin-dev <3489181720@qq.com>
2026-03-08 19:13:19 +0800

    命令模式入门Demo

---
+++ wangbin-generator-basic/src/main/java/com/wangbing/cli/pattern/Command.java
@@ -0,0 +1,6 @@
+package com.wangbing.cli.pattern;
+
+public interface Command {
+    void execute();
+}
+
---
+++ wangbin-generator-basic/src/main/java/com/wangbing/cli/pattern/RemoteControl.java
@@ -0,0 +1,13 @@
+package com.wangbing.cli.pattern;
+
+public class RemoteControl {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+    }
+}
